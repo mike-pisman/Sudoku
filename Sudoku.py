75e60ad0fd3cf2e2e180761595eb98048015226a
Mikhail Pisman <mike.pisman@gmail.com>
2020-06-29 14:21:30 -0700

    Reserve copy

--- Sudoku.py
+++ Sudoku.py
@@ -10,49 +10,59 @@ class Sudoku:
     #Function generates board 9x9 with random numbers
     def generate_board(self):
 
+        square = []
+        for i in range(9):
+            square.append(np.zeros((3,3)))
+
         # Top left square
-        first_sq = np.random.choice(range(1,10), 9, replace=False)
-        first_sq = first_sq.reshape(3,3)
+        square[0] = np.random.choice(range(1,10), 9, replace=False)
+        square[0] = square[0].reshape(3,3)
 
         # Top middle square
-        second_sq = np.zeros((3,3), dtype=int)
+        square[1] = np.zeros((3,3), dtype=int)
 
-        elements = np.setdiff1d(range(1,10), first_sq[0])
-        second_sq[0] = np.random.choice(elements, 3, replace=False)
+        elements = np.setdiff1d(range(1,10), square[0][0])
+        square[1][0] = np.random.choice(elements, 3, replace=False)
 
-        elements = np.setdiff1d(range(1,10), first_sq[1])
-        elements = np.setdiff1d(elements, second_sq)
-        test = np.intersect1d(elements, first_sq[2], assume_unique=True)
+        elements = np.setdiff1d(range(1,10), square[0][1])
+        elements = np.setdiff1d(elements, square[1])
+        test = np.intersect1d(elements, square[0][2], assume_unique=True)
 
         if len(test) < 3:
             elements = np.setdiff1d(elements, test)
             elements = np.random.choice(elements, 3 - len(test), replace=False)
             test = np.append(test, elements)
 
-        second_sq[1] = test
+        square[1][1] = test
 
-        elements = np.setdiff1d(range(1,10), second_sq)
-        second_sq[2] = np.random.choice(elements, 3, replace=False)
+        elements = np.setdiff1d(range(1,10), square[1])
+        square[1][2] = np.random.choice(elements, 3, replace=False)
 
-        # Top middle square
-        third_sq = np.zeros((3,3), dtype=int)
+        # Top right square
+        square[2] = np.zeros((3,3), dtype=int)
         for i in range(3):
-            elements = np.setdiff1d(range(1,10), np.append(first_sq[i], second_sq[i]))
-            third_sq[i] = np.random.choice(elements, 3, replace=False)
+            elements = np.setdiff1d(range(1,10), np.append(square[0][i], square[1][i]))
+            square[2][i] = np.random.choice(elements, 3, replace=False)
 
+        # Middle left square
+        square[3] = np.zeros((3,3), dtype=int)
+        for i in range(3):
+            elements = np.setdiff1d(range(1,10), np.append(square[0][i], square[1][i]))
+            square[2][i] = np.random.choice(elements, 3, replace=False)
 
         for i in range(3):
-            print(first_sq[i], second_sq[i], third_sq[i])
+            print(square[0][i], square[1][i], square[2][i])
 
     def print_board(self):
         print( *(self.board), sep = "\n" )
 
 def main():
-    start = time.time()
-    game = Sudoku()
-    print('\n executed in', time.time() - start, 'second')
+    cleargame = Sudoku()
+
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    print('\n executed in', time.time() - start, 'second')
 
 #game.print_board()
